1d5342acf8ce989f214ca903682efb9cadaecb80
macyramos <macytramos@gmail.com>
2024-08-14 10:32:53 -1000

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,17 @@
+/* 
+ * I  did all the work.
+ * Not Aden. (this is a lie)
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Task 1: Print out "Hello World"
+		System.out.print("Hello World!");
+		//Task 2: Print out "Hello World"
+		System.out.println("Hello World!");
+		//Task 2: Print it again on the next line
+		System.out.print("Hello World!");
+	}
+	
+}
