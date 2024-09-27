ce5b6f4877c2d3e1c07b80771c8edbaa5075743e
macyramos <macytramos@gmail.com>
2024-09-27 12:39:53 -1000

    Update Triangle.java

--- Projects/Shapes/Triangle.java
+++ Projects/Shapes/Triangle.java
@@ -1,1 +1,67 @@
+/**
+ * This is my triangles class.
+ *There are the components that make up  a triangle.
+ */
+public class Triangle {
+	
+	//data attributes
+/**
+ * This is the  attributes that make up a  triangle.	
+ */
+		private double side;
+		
+		
+	//constructor
+/**
+ * 	This is where the components of making a triangle  is put.
+ */
+		public Triangle (double s)
+		{
+			side = s;
+		}
+		
+		
+		//functionality
+/**
+ * This is  where the side gets  returned
+ * @return
+ */
+		public double getSide()
+		{
+			return side;
+		}
+			
+/**
+ * This is where the perimeter gets  returned.
+ * @return
+ */
+							
+		public double getPerimeter() 
+		{
+			return 3*side;
+		}
 
+/**
+ * This is where the area gets  returned
+ * @return
+ */
+		public double getArea() 
+		{
+			int sqrt = 3;
+			return Math.sqrt(sqrt) / 4 * side * side;
+		}
+/**
+ * This is where the triangle shape gets printed.			
+ * @return
+ */
+			
+		public static String getShape() 
+		{
+		return "Triangle:";
+		}
+
+
+public void setSide(double side) {
+	this.side = side;
+}
+	}
