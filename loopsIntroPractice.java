3aacc3a81715ac7b4f297adc2fffd3065ae0354b
macyramos <macytramos@gmail.com>
2024-11-13 08:48:27 -1000

    Create loopsIntroPractice.java

---
+++ loopsIntroPractice.java
@@ -0,0 +1,16 @@
+@author Macy Ramos
+
+public class loopsIntroPractice {
+
+  public static void main (String [] args) {
+  }
+  
+  public static int countLetters(String str, char let)
+  {
+    int count = 0;
+    for (int i = 0; i < str.length(); i ++;)
+      
+      {
+        System.out.println(str.charAt(i));
+      }  
+  }
