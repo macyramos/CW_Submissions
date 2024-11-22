55bb73075d5de99ca195a94ee67cb6726747aa1f
macyramos <macytramos@gmail.com>
2024-11-22 12:30:51 -1000

    Update loopsIntroPractice.java

--- loopsIntroPractice.java
+++ loopsIntroPractice.java
@@ -1,16 +1,23 @@
 @author Macy Ramos
 
-public class loopsIntroPractice {
-
-  public static void main (String [] args) {
+ public static void main (String [] args) {
+	  System.out.println("abaac");
+	  System.out.println("");
+	  new String = str();
+	 
   }
   
-  public static int countLetters(String str, char let)
+ public static int countLetters(String str, char let)
   {
     int count = 0;
-    for (int i = 0; i < str.length(); i ++;)
+    for (int i = 0; i < str.length(); i ++)
       
       {
-        System.out.println(str.charAt(i));
-      }  
-  }
+    	if(let == str.charAt(i))
+    	{
+    		count++ ;
+    	}
+    	
+      } 
+    	return count;
+
