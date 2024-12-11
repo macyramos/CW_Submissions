d0c6831b6512ee122682514376c0b64651cabe38
macyramos <macytramos@gmail.com>
2024-12-11 09:33:54 -1000

    Update logicQuizCorrection.java

--- logicQuizCorrection.java
+++ logicQuizCorrection.java
@@ -1,18 +1,20 @@
 @author Macy Ramos
+
+
+	
 public class logicQuizCorrection {
 
 	public static void main(String[] args) {
+		System.out.println(findIfIsLeapYear(100));
+		System.out.println(findIfIsLeapYear(1001));
+		System.out.println(findIfIsLeapYear(400));
+		System.out.println(findIfIsLeapYear(412));
 		
 	}
-		// TODO Auto-generated method stub
 
 	public static boolean findIfIsLeapYear (int year) {
 	
-		if(year%100==0 && year%400==0)
-		{
-			return true;
-		}
-		else if(year % 4==0)
+		if(year%400==0 || year%4==0 && year % 100!=0)
 		{
 			return true;
 		}
