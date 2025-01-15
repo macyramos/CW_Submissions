b4468c829cf06fe30b276d85117e679dec85c014
macyramos <macytramos@gmail.com>
2025-01-15 10:11:13 -1000

    Create ArrayIntroPrac.java

---
+++ ArrayIntroPrac.java
@@ -0,0 +1,38 @@
+Today you will be declaring variables that store an array of elements. Create code that accomplishes the following tasks and submit it via github:
+
+1. Create an empty array that is meant to represent doubles. 
+Name it doubArr. Repeat this for arrays that represent ints, booleans, String objects, and Circle objects.
+
+double [] doubleArr;
+{}
+int [] intArr;
+{}
+boolean [] booArr;
+{}
+String [] strArr;
+{}
+circ [] circArr;
+{}
+
+2. Create an array of ints with the following represented values: 7, -12, 88, -88, 0, 5, 2000.
+
+int [] intArr = {7, -12, 88, -88, 0, 5, 2000}
+
+3. Create an array that represents 5 String objects with default values. 
+//Name the array fruitsArr. In the comment for this task, state what each element's default value is. 
+//State what the default value for ints, doubles, booleans, and any reference data type is as well. 
+  
+String[] fruitsArr = new String [5];
+System.out.println {"Apple", "Orange", "Banana", "Watermelon", "Strawberry"};
+  
+4. Change the 2nd element in fruitsArr to "Bapple".
+ x;
+int[] fruitsArr = 
+  
+
+5. Change the last element in fruitsArr to "Zanana" . 
+//Accomplish this without typing the number "4" and instead accessing the arrays length.
+
+6. To verify you did number 4 correctly, print out the second element of fruitsArr.
+
+7. Create a loop that one by one prints out each element in fruitsArr.
