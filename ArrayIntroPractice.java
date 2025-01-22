5bc4b00d3e11846f5af1a1b99311f573735c066b
macyramos <macytramos@gmail.com>
2025-01-22 09:54:29 -1000

    Update ArrayIntroPractice.java

--- ArrayIntroPractice.java
+++ ArrayIntroPractice.java
@@ -1,8 +1,9 @@
-Today you will be declaring variables that store an array of elements. Create code that accomplishes the following tasks and submit it via github:
+//Today you will be declaring variables that store an array of elements. Create code that accomplishes the following tasks and submit it via github:
 
-1. Create an empty array that is meant to represent doubles. 
-Name it doubArr. Repeat this for arrays that represent ints, booleans, String objects, and Circle objects.
-
+/*
+*1. Create an empty array that is meant to represent doubles. 
+*Name it doubArr. Repeat this for arrays that represent ints, booleans, String objects, and Circle objects.
+*/
 double [] doubleArr;
 {}
 int [] intArr;
@@ -14,25 +15,43 @@ String [] strArr;
 circ [] circArr;
 {}
 
-2. Create an array of ints with the following represented values: 7, -12, 88, -88, 0, 5, 2000.
-
+/*
+ * 2. Create an array of ints with the following represented values: 7, -12, 88, -88, 0, 5, 2000.
+*/
 int [] intArr = {7, -12, 88, -88, 0, 5, 2000}
 
-3. Create an array that represents 5 String objects with default values. 
-//Name the array fruitsArr. In the comment for this task, state what each element's default value is. 
-//State what the default value for ints, doubles, booleans, and any reference data type is as well. 
+/*
+*3. Create an array that represents 5 String objects with default values. 
+*Name the array fruitsArr. In the comment for this task, state what each element's default value is. 
+*State what the default value for ints, doubles, booleans, and any reference data type is as well. 
+*/
   
 String[] fruitsArr = new String [5];
-System.out.println {"Apple", "Orange", "Banana", "Watermelon", "Strawberry"};
+System.out.println {"null", "Apple", "null", "null", "Banana"};
+
+/*
+*4. Change the 2nd element in fruitsArr to "Bapple".
+*/
+  x;
+ fruitsArr [1] = 'Bapple';
   
-4. Change the 2nd element in fruitsArr to "Bapple".
- x;
-int[] fruitsArr = 
-  
+/*
+*5. Change the last element in fruitsArr to "Zanana" . 
+*Accomplish this without typing the number "4" and instead accessing the arrays length.
+*/
+  fruitsArr [fruits.length-1] = "Zanana";
 
-5. Change the last element in fruitsArr to "Zanana" . 
-//Accomplish this without typing the number "4" and instead accessing the arrays length.
+/*    
+*6. To verify you did number 4 correctly, print out the second element of fruitsArr.
+*/ 
+    System.out.println(fruitsArr[1]);
 
-6. To verify you did number 4 correctly, print out the second element of fruitsArr.
+/*
+ *7. Create a loop that one by one prints out each element in fruitsArr.
+*/
 
-7. Create a loop that one by one prints out each element in fruitsArr.
+
+  for(int i=0; i< arr.length; i++);
+{
+ System.out.println(arr[i]); 
+}
